6c9bc668b95ccce7a1e8f438e4d3b9a0d09b4d14
SanteriV <54982640+SvenZerkov@users.noreply.github.com>
2023-02-07 11:43:50 +0200

    add the initial directory tree batch file

---
+++ luonti.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+md "EsimerkkiHakemisto"
+
+cd EsimerkkiHakemisto
+
+md "Yritys Oy"
+
+cd Yritys Oy
+
+md "Hallinto" "Myyntikonttori" "Talouskonttori"
+
+cd Talouskonttori
+
+md "1_Projekti" "2_Projekti" "3_Projekti" "4_Projekti"
